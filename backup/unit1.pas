5884dc81f0b49e565b1329d78677765baf72d22b
hafiz1704 <hafizansyari856@gmail.com>
2020-05-02 23:08:04 +0700

    operasi tipe data

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -13,6 +13,8 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    eNama: TEdit;
+    Label1: TLabel;
     procedure Button1Click(Sender: TObject);
   private
 
@@ -30,8 +32,32 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
+var
+  namadepan: string;
+  namatengah, namabelakang: string;
+  usia: integer;
+  targetkuliah: integer;
+  IPK: double;
+  nilaiabjad: char;
+  tampan: boolean;
 begin
-  showmessage('hello world');
+     namadepan:='hafiz';
+     namatengah:='ansyari';
+     namabelakang:='rantung';
+     usia:=19;
+     IPK:= 3.896572431;
+     nilaiabjad:='A';
+     tampan:=false;
+
+     messageDlg('judul pesan',namadepan + ' ' +
+           namatengah + ' ' + namabelakang, mtInformation,[mbOK],0);
+
+     messageDlg('usia',inttostr(usia), mtInformation,[mbOK],0);
+
+     messageDlg('tampan',booltostr(tampan), mtInformation,[mbOK],0);
+
+     messageDlg('judul pesan','isi pesan',mtInformation,[mbOK],0);
+
 end;
 
 end.
--- unit1.pas
+++ unit1.pas
@@ -32,8 +32,34 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
+var
+  namadepan: string;
+  namatengah, namabelakang: string;
+  usia: integer;
+  targetkuliah: integer;
+  IPK: double;
+  nilaiabjad: char;
+  tampan: boolean;
 begin
-  showmessage('hello '+eNama.text);
+     namadepan:='hafiz';
+     namatengah:='ansyari';
+     namabelakang:='rantung';
+     usia:=19;
+     IPK:= 3.896572431;
+     nilaiabjad:='A';
+     tampan:=false;
+
+     messageDlg('judul pesan',namadepan + ' ' +
+           namatengah + ' ' + namabelakang, mtInformation,[mbOK],0);
+
+     messageDlg('usia',inttostr(usia), mtInformation,[mbOK],0);
+
+     messageDlg('IPK',floattostrf(IPK,fffixed,3,2), mtInformation,[mbOK],0);
+
+     messageDlg('tampan',booltostr(tampan), mtInformation,[mbOK],0);
+
+     messageDlg('hari ini',formatdatetime('dddd, dd-mmmm-yyyy',now), mtInformation,[mbOK],0);
+
 end;
 
 end.
